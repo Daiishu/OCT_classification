85b5f1bfba6894434488e7c27e61cafc127b82da
Daishu <dawidziniewicz@Dawids-MacBook-Pro-2.local>
2022-07-17 13:46:36 +0200

    data

---
+++ data_loader.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+from pathlib import Path
+
+# class OCT_data(tf.keras.utils.Sequence):
+#     def __init__(self):
+#     def __len__():
+#     def __getitem__():
+#     return images, classes
