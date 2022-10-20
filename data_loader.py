1b52e996fc72ac6430f0a71d4e1f553e5eb85b26
Daiishu <88497502+Daiishu@users.noreply.github.com>
2022-10-20 09:57:22 +0200

    Delete data_loader.py

--- data_loader.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-from pathlib import Path
-
-# class OCT_data(tf.keras.utils.Sequence):
-#     def __init__(self):
-#     def __len__():
-#     def __getitem__():
-#     return images, classes
